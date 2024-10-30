e2dc0146560b56fc84f6142effa5341321c7f8c0
Max <mshevchenko1984@gmail.com>
2024-10-30 15:18:54 +0200

    Add converter F to C

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter F to C");
+
+        int F = 68;
+        int C = (F - 32) * 5 / 9;
+        System.out.println(F + " Fahrenheits will be " + C + " Celsius");
     }
 }
